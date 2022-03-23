181e64b3e77319cb9576ce52826df9e37368420d
iterhim <bezyc11@gmail.com>
2022-03-23 20:51:25 +0200

    last

---
+++ const.js
@@ -0,0 +1,17 @@
+const command = `
+/start - перезапустить бота
+/help - помощь
+`
+const text1 = `
+1Текст для провірки обработчика і <a href="https://youtube.com">ссилка</a>
+`
+const text2 = `
+2Текст для провірки обработчика і <a href="https://youtube.com">ссилка</a>
+`
+const text3 = `
+3Текст для провірки обработчика і <a href="https://youtube.com">ссилка</a>
+`
+module.exports.command = command;
+module.exports.text1 = text1;
+module.exports.text2 = text2;
+module.exports.text3 = text3;
